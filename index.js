dc3b1118f62db767c3831bc221b5e02243298166
PedroAugcardozo <pedroaug.cardozo@gmail.com>
2025-02-12 12:36:01 -0300

    inicio da coleta dos dados da tabela de faltas

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 async function Robo() {
     // - abre a pagina do aprender
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         defaultViewport: {
             width: 1920,
             height: 1080
@@ -14,6 +14,7 @@ async function Robo() {
     const page = await browser.newPage();
     const url = `https://www.unoeste.br/site/ava/`
     await page.goto(url)
+    console.log('abriu o navegador')
 
     // - pega o RA e a senha do usuario
     const RA = process.env.RA_KEY
@@ -28,14 +29,48 @@ async function Robo() {
     // - entra na area do aluno
     await page.waitForSelector('.infacad')
     await page.click('.infacad')
+    console.log('area do aluno acessada')
 
     // - entra na pagina de faltas e notas
     await page.waitForSelector('[href="https://www.unoeste.br/site/consnota/aluno/ConsultaNota.aspx"]')
     await page.click('[href="https://www.unoeste.br/site/consnota/aluno/ConsultaNota.aspx"]')
 
     // - entra em faltas
-    await page.waitForSelector('.linkNoHT')
-    await page.click('.linkNoHT')
+    await page.waitForSelector('[data-index="1"]')
+    await page.click('[data-index="1"]')
+    console.log('area de faltas acessada')
 
+    //pega as materias
+
+    await page.waitForSelector('table', { timeout: 5000 }); // Espera até 5 segundos
+
+    let dados = await page.$$eval('table tr', linhas => {
+        return linhas
+            .filter(linha => {
+                // Filtra linhas que têm pelo menos uma coluna com dados
+                const colunas = linha.querySelectorAll('td.center span');
+                return colunas.length > 0;
+            })
+            .map(linha => {
+                const materia = linha.getAttribute('data-disnome')?.trim() || '';
+                
+                const colunas = linha.querySelectorAll('td.center span');
+                const faltas = colunas.length > 0 ? colunas[colunas.length - 1].innerText.trim() : '';
+    
+                // Verifica se a string de faltas é um número ou uma porcentagem
+                const isPercentage = /^\d{1,3}%$/.test(faltas);
+    
+                return { 
+                    disciplina: materia, 
+                    perc_faltas: isPercentage ? faltas : 'CURSANDO' 
+                };
+            });
+    });
+    // Convertendo os dados filtrados de volta para um array
+    dados = Array.from(dados.values());
+
+    console.log(dados)
+
+    browser.close
 }
 Robo()
