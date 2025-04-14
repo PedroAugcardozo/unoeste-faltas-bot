838c19bec511939f468dec17b1b9544208df4bd2
PedroAugcardozo <pedroaug.cardozo@gmail.com>
2025-04-14 19:19:05 -0300

    {mensagem_do_commit}

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { timeout } = require('puppeteer');
 const puppeteer = require('puppeteer')
 const readLineSync = require('readline-sync')
 require('dotenv').config();
@@ -6,47 +7,52 @@ require('dotenv').config();
 //? quando está pronto
 //! quando pode ser que tenha uma alteração no futuro
 
-async function Robo() {
-    //? - abre a pagina do aprender
-    const browser = await puppeteer.launch({
-        headless: true,
-        defaultViewport: {
-            width: 1920,
-            height: 1080
-        }
-    })
-    const page = await browser.newPage();
-    const url = `https://www.unoeste.br/site/ava/`
-    await page.goto(url)
-    console.log('abriu o navegador')
+async function Robo() { 
+    try{
+        //? - abre a pagina do aprender
+        const browser = await puppeteer.launch({
+            headless: true,
+            defaultViewport: {
+                width: 1920,
+                height: 1080
+            }
+        })
+        const page = await browser.newPage();
+        const url = `https://www.unoeste.br/site/ava/`
+        await page.goto(url)
+        console.log('abriu o navegador')
 
-    //? - pega o RA e a senha do usuario
-    const RA = process.env.RA_KEY
-    const Senha = process.env.PASS
+        //? - pega o RA e a senha do usuario
+        const RA = process.env.RA_KEYj
+        const Senha = process.env.PASS
 
-    //? - digita e cadastra o RA e a senha do usuario e entra no aprender 
-    await page.type('[name="tbLogin"]', RA)
-    await page.type('[name="tbSenha"]', Senha)
-    await page.click('[type="submit"]')
-    console.log('aprender acessado')
+        //? - digita e cadastra o RA e a senha do usuario e entra no aprender 
+        await page.type('[name="tbLogin"]', RA)
+        await page.type('[name="tbSenha"]', Senha)
+        await page.click('[type="submit"]')
+        console.log('aprender acessado')
 
-    //? - entra na area do aluno
-    await page.waitForSelector('.infacad')
-    await page.click('.infacad')
-    console.log('area do aluno acessada')
+        //? - entra na area do aluno
+        await page.waitForSelector('.infacad')
+        await page.click('.infacad')
+        console.log('area do aluno acessada')
 
-    //? - entra na pagina de faltas e notas
-    await page.waitForSelector('[href="https://www.unoeste.br/site/consnota/aluno/ConsultaNota.aspx"]')
-    await page.click('[href="https://www.unoeste.br/site/consnota/aluno/ConsultaNota.aspx"]')
+        //? - entra na pagina de faltas e notas
+        await page.waitForSelector('[href="https://www.unoeste.br/site/consnota/aluno/ConsultaNota.aspx"]')
+        await page.click('[href="https://www.unoeste.br/site/consnota/aluno/ConsultaNota.aspx"]')
 
-    //? - entra em faltas
-    await page.waitForSelector('[data-index="1"]')
-    await page.click('[data-index="1"]')
-    console.log('area de faltas acessada')
+        //? - entra em faltas
+        await page.waitForSelector('[data-index="1"]')
+        await page.click('[data-index="1"]')
+        console.log('area de faltas acessada')
+    }catch(error){
+        console.error('ERRO NO PROCESSO', error.message)
+    }
 
     //!pega as materias
 
-    await page.waitForSelector('table', { timeout: 5000 }); // Espera até 5 segundos
+    await page.waitForSelector('table')
+    
 
     let dados = await page.$$eval('table tr', linhas => {
         return linhas.map(linha => {
@@ -64,7 +70,8 @@ async function Robo() {
     
             const totalFaltas = presencialNum + naoPresencialNum;
             const percFaltas = chPrevistaNum > 0 ? ((totalFaltas / chPrevistaNum) * 100).toFixed(2) + "%" : "0%";
-            //calcula quantas faltas ainda tenho
+            
+            //! calcula quantas faltas ainda tenho
             const faltasPermitidas = (chPrevistaNum * 25) / 100
             const horasDeFalta = presencialNum + naoPresencialNum
             const horasRestantes = faltasPermitidas - horasDeFalta
